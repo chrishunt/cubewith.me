1f997e275b8e61abf93f050f40aabac371ab4a23
Chris Hunt <huntca@gmail.com>
2011-11-09 09:30:47 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cuberoom::Application.config.secret_token = 'c91f66605b52c2960542a4ffe5c5cc64ce730719cff4e58a592fa67e2e6bbbdc23c891c8e8f551730558ac0204559b951126fc5eeb73e276d601b86e9638b12a'
